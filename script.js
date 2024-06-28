3a1759288f8178a7d634bc2d596b72fc4bc157b8
Mehdi <mghayoum@binghamton.edu>
2024-06-27 21:50:29 -0700

    Update HTML, CSS, and JS for better visualization and image display

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
     webgazer.setRegression('ridge')
         .setTracker('clmtrackr')
         .begin()
-        .showVideoPreview(true) // This will append the video element automatically
+        .showVideoPreview(true)
         .showPredictionPoints(true)
         .applyKalmanFilter(true);
 
@@ -18,11 +18,28 @@ window.onload = function() {
         saveGazeData({ x: xprediction, y: yprediction, time: elapsedTime });
     }).begin();
 
-    // Move video element to specific location
-    const video = document.querySelector('video');
-    const videoContainer = document.getElementById('webcamVideo');
-    videoContainer.parentNode.replaceChild(video, videoContainer);
-    video.id = 'webcamVideo';
+    // Display images and collect gaze data
+    const images = [
+        'image1.jpg',
+        'image2.jpg',
+        'image3.jpg',
+        // Add more image paths
+    ];
+
+    let currentImageIndex = 0;
+    const demoImage = document.getElementById('demoImage');
+
+    function showNextImage() {
+        if (currentImageIndex < images.length) {
+            demoImage.src = images[currentImageIndex];
+            currentImageIndex++;
+            setTimeout(showNextImage, 5000); // Show each image for 5 seconds
+        } else {
+            alert('Image display complete. Gaze data collection finished.');
+        }
+    }
+
+    showNextImage();
 };
 
 function saveGazeData(data) {
