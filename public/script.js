c6731d2717d9d4054f018395652fe63aa0a4cf54
Mehdi <mghayoum@binghamton.edu>
2024-06-30 19:57:29 -0700

    Add images and update paths in JavaScript

--- public/script.js
+++ public/script.js
@@ -1,26 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+window.onload = async function() {
+    await webgazer.setRegression('ridge')
+        .setTracker('clmtrackr')
+        .begin()
+        .showVideoPreview(true)
+        .showPredictionPoints(true)
+        .applyKalmanFilter(true);
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+    webgazer.setGazeListener(function(data, elapsedTime) {
+        if (data == null) {
+            return;
+        }
+        var xprediction = data.x;
+        var yprediction = data.y;
+        document.getElementById('gazeData').innerHTML = 'X: ' + xprediction + ' Y: ' + yprediction;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'gaze.html'));
-});
+        // Send gaze data to the server
+        saveGazeData({ x: xprediction, y: yprediction, time: elapsedTime });
+    }).begin();
 
-app.post('/save-gaze-data', (req, res) => {
-    const data = req.body;
-    fs.appendFile('gazeData.json', JSON.stringify(data) + '\n', (err) => {
-        if (err) throw err;
-        console.log('Gaze data saved!');
+    // Display images and collect gaze data
+    const images = [
+        'images/image1.jpg',
+        'images/image2.jpg',
+        'images/image3.jpg',
+        'images/image4.jpg',
+        'images/image5.jpg',
+        'images/image6.jpg',
+        'images/image7.jpg',
+        'images/image8.jpg',
+        'images/image9.jpg',
+        'images/image10.jpg'
+    ];
+
+    let currentImageIndex = 0;
+    const demoImage = document.getElementById('demoImage');
+
+    function showNextImage() {
+        if (currentImageIndex < images.length) {
+            demoImage.src = images[currentImageIndex];
+            currentImageIndex++;
+            setTimeout(showNextImage, 5000); // Show each image for 5 seconds
+        } else {
+            alert('Image display complete. Gaze data collection finished.');
+        }
+    }
+
+    showNextImage();
+};
+
+function saveGazeData(data) {
+    fetch('/save-gaze-data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     });
-    res.status(200).send('Data received');
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+}
